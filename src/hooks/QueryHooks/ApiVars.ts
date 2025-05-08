a6753b0dbf0d76515af83321877f50981cfd1dc5
Alexander Proshanov <proshanov@yandex.ru>
2025-05-08 17:22:30 +0300

    build: Add remote api

--- src/hooks/QueryHooks/ApiVars.ts
+++ src/hooks/QueryHooks/ApiVars.ts
@@ -1,4 +1,7 @@
-export const BASE_API = "http://localhost:3001";
+export const BASE_API =
+  process.env.NODE_ENV === "production"
+    ? "https://jsonapi.metabroadcast.ru"
+    : "http://localhost:3001";
 export const USERS = `${BASE_API}/users`;
 export const LOGIN = `${BASE_API}/login`;
 export const GET_SOS = `${BASE_API}/660/sos`;
