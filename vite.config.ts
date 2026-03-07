a34d12d76df2f68bac648f645d0d6b8e6ac91b89
magrega <proshanov@yandex.ru>
2026-03-07 16:34:21 +0300

    build: Change chunkSizeWarningLimit to 800

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: { open: true },
+  build: {
+    chunkSizeWarningLimit: 800, // kB
+  },
 });
